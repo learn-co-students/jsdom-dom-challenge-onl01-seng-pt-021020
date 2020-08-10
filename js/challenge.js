948719210b7b084e811cb8415614dd69afd6e653
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-08-10 15:17:38 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,22 @@
 window.addEventListener('load', (event) => {
     console.log('page is fully loaded');
-});
 
+// getting el from DOM
  counter = parseInt(document.getElementById('counter').innerText)
 
+//incrementing counter
+counter++ 
+});
 
+const intervalID = setInterval(function() { 
+    myCallback('one', 'two'); }, 1000);
 
-// document.addEventListener
+    function myCallback(a, b)
+{
+
+    console.log(a)
+    console.log(b)
+
+} 
+
+window.clearInterval(intervalID)
